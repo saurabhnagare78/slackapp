2be4ae2974ac9db05b17d3391eba8d2010ff4233
Faizan Qazi <fqazi@wesfarmers.com.au>
2022-06-08 10:58:34 +0530

    presence set

--- app_autoresp.py
+++ app_autoresp.py
@@ -2,10 +2,17 @@ import os
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 import datetime
+import json
 
 # Initializes your app with your bot token and socket mode handler
 app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
-@app.event("message") # message is an event handler refer: https://api.slack.com/events
+USER_TOKEN = os.environ.get("SLACK_USER_TOKEN")
+
+def pprint(response):
+    print(json.dumps(response,indent=4))
+
+# message is an event handler refer: https://api.slack.com/events
+@app.event("message") 
 def respond(event, say, context, client, body ):
     sender = event["user"]
     receiver = body["authorizations"][0]["user_id"]
@@ -19,17 +26,26 @@ def respond(event, say, context, client, body ):
         dt = datetime.datetime.fromtimestamp(expiration)
         text = f"Hi, <@{ sender }>!!!\nI am Out of Office and will be back on {dt}"
         app.client.chat_postMessage(
-            token = client.token, 
+            # response from Bot
+            # token = client.token, 
+            # username = user_info["name"],
+            # icon_url = user_info["profile"]["image_24"],
+            token = USER_TOKEN,
             channel = sender,
             text = text,
-            username = user_info["name"],
-            icon_url = user_info["profile"]["image_24"],
         )
 
 @app.event("user_status_changed")
-def handle_user_status_changed_events(body, logger):
-    print(body)
+def handle_user_status_changed_events(body, logger, event):
+    # user = event["user"]["id"]
+    status = event["user"]["profile"]["status_text"]
     # If status changed to OOO set presence as inactive
+    try:
+        if status == "Out of Office":
+            print(app.client.users_setPresence(token = USER_TOKEN,presence = "away"))
+    except Exception as err:
+        print(err)
+    # print(json.dumps(body, indent = 4))
 
 
 # Start your app
